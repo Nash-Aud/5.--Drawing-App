65fd9a1f0a9ae210832894268fa44bcc934dad0a
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-03-14 18:05:24 -0300

    Finished all the functionalities

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const canvas = document.querySelector('canvas'),
-toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelector("#fill-color"),
-ctx = canvas.getContext('2d');
+      toolBtns = document.querySelectorAll(".tool"),
+      fillColor = document.querySelector("#fill-color"),
+      sizeSlider = document.querySelector("#size-slider"),
+      colorBtns = document.querySelectorAll(".colors .option"),
+      colorPicker = document.querySelector("#color-picker"),
+      clearCanvas = document.querySelector(".clear-canvas"),
+      saveImg = document.querySelector(".save-img"),
+      ctx = canvas.getContext('2d');
 
 //global variables with default value
 let prevMouseX, prevMouseY, snapshot,
     isDrawing = false,
     selectedTool = "brush",
     brushWidth = 5;
+    selectedColor = "#000";
+
+const setCanvasBackground = () => {
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = selectedColor;
+};
 
 //setting canvas width/height 
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    setCanvasBackground();
 });
 
 const drawRect = (e) => {
@@ -38,7 +51,7 @@ const drawTriangle = (e) => {
     ctx.lineTo(e.offsetX, e.offsetY);//first line of triangle according mouse pointer
     ctx.lineTo(prevMouseX *2 - e.offsetX, e.offsetY); //bottom line of triangle
     ctx.closePath(); //closing path of triangle
-    ctx.stroke();
+    fillColor.checked ? ctx.fill() : ctx.stroke();
 }
 
 const startDraw = (e) => {
@@ -47,6 +60,8 @@ const startDraw = (e) => {
     prevMouseY = e.offsetY; //passing current mouseY position as prevMouseY value   
     ctx.beginPath(); //starts a new drawing
     ctx.lineWidth = brushWidth; //passing brushSize as lineWidth
+    ctx.strokeStyle = selectedColor; //passing selectedcolor as stroke style
+    ctx.fillStyle = selectedColor; //passing selectedcolor as fill style
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height); //copies pixel data 
 
 }
@@ -55,7 +70,8 @@ const drawing = (e) => {
     if(!isDrawing) return; //if isDrawing is false return from here
     ctx.putImageData(snapshot, 0, 0); //put imagedata back to canvas
 
-    if(selectedTool === "brush") {
+    if(selectedTool === "brush" || selectedTool === "eraser") {
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
     //creating line according to the mouse poiner
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke(); //drawing line with color        
@@ -77,6 +93,39 @@ toolBtns.forEach(btn => {
         console.log(selectedTool);
     })
 });
+
+//passing slider value as brushsize
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => { //click event on each color
+        //removing selected class from the previous option, and adding on current clicked option
+        document.querySelector(".options .selected").classList.remove("selected");
+        btn.classList.add("selected");
+        //passing selected btn background color as selected color value
+        selectedColor = (window.getComputedStyle(btn).getPropertyValue("background-color"));
+    });
+});
+
+colorPicker.addEventListener("change", () => {
+    //passing color value from color picker to last color btn background
+    colorPicker.parentElement.style.background = colorPicker.value;
+    colorPicker.parentElement.click();
+});
+
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setCanvasBackground();
+});
+
+saveImg.addEventListener("click", () => {
+    const link = document.createElement("a"); //creating <a> element
+    link.download = `${Date.now()}.jpg`; //passing current date as link download value
+    link.href = canvas.toDataURL(); //passing canvasData as link href value
+    link.click(); //clicking link to download image
+});
+
+
 
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mouseup", () => isDrawing = false);
